2786998246ec2328bbedc15c811d97d9b5ac6054
mdjibril <muhammadjibrildauda@gmail.com>
2019-11-26 12:22:59 +0100

    addedd text

--- using-express/app.js
+++ using-express/app.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express()
 
 // A sample route
-app.get('/', (req, res) => res.send('Hello World! via EXPRESS'))
+app.get('/', (req, res) => res.send('Hello World! via EXPRESS '))
 
 // Start the Express server
 app.listen(3000, () => console.log('Server running on port 3000!'))
