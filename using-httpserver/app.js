e87b587514de6d3eddacee7615802f0bdcb2fe05
mdjibril <muhammadjibrildauda@gmail.com>
2019-11-26 09:54:10 +0100

    Initial commit

---
+++ using-httpserver/app.js
@@ -0,0 +1,10 @@
+// app.js
+
+const server = require('server');
+
+const { get, post } = server.router;
+
+// Launch server
+server({ port: 3000 }, [
+    get('/', ctx => 'Hello world! through SERVER')
+]);
